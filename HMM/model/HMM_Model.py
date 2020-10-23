02553a8ed63076084d9d02350502b0db75d70ae6
wry <wry@qq.com>
2020-10-23 14:13:31 +0800

    添加HMM 中文分词、中文词性标注example； 添加SVD 推荐系统example

--- HMM/model/HMM_Model.py
+++ HMM/model/HMM_Model.py
@@ -37,8 +37,10 @@ class HiddenMarkov:
         # 要计算各个时刻的各个隐藏状态的前向概率
         T = M # 时刻数
         # 算法原理 https://s1.ax1x.com/2020/10/21/B9r63Q.png
+        # 代码逻辑 https://s1.ax1x.com/2020/10/21/BCuJiT.png
         for t in range(T):
             index_O = V.index(O[t]) #当前观测状态在观测集合中的索引
+            print(f"time {t + 1} and observe state is {O[t]}")  # 因为这边python的索引是从0开始的
             for i in range(N): # 计算各个隐藏状态的前向概率,
                 if t==0:
                     alphas[i,t] = PI[i] * B[i,index_O]
@@ -48,21 +50,65 @@ class HiddenMarkov:
                 # 隐藏状态3  [0.7, 0.3]]
                 else:
                     alphas[i,t] = np.dot([alpha[t - 1] for alpha in alphas],[a[i] for a in A]) * B[i,index_O]
-                    # https://s1.ax1x.com/2020/10/21/BCuJiT.png
+                if t == T-1:
+                        print(f"time {t+1} state {Q[i]} alpha is {alphas[i, t]:.4f}")
 
         self.forword_p = np.sum([alpha[M - 1] for alpha in alphas])
         self.alphas = alphas
+        print(f"finally p is {self.forword_p:.4f}")
 
     def backward(self, Q, V, A, B, O, PI):
         N = len(Q) # 隐藏状态序列长度
         M = len(O) # 观测状态序列长度
-        betas = np.zeros((N,M)) # 初始化后向概率
-        #算法原理 https://s1.ax1x.com/2020/10/21/BC1AC6.png
-
+        betas = np.ones((N,M)) # 初始化后向概率
+        # 算法原理 https://s1.ax1x.com/2020/10/21/BC1AC6.png
+        # 代码逻辑 https://s1.ax1x.com/2020/10/21/BPkLGj.jpg
         for t in range(M - 2, -1, -1): # 对观测序列逆向遍历
             index_O = V.index(O[t+1])  # 观测为t+1的
+            print(f"time {t+1} and observe state is {O[t+1]}") # 因为这边python的索引是从0开始的
             for i in range(N): # 通过t+1时刻N个状态的后向概率计算t时刻i状态的后向概率
-                betas[i,t] = np.dot()
+                betas[i,t] = np.dot(np.multiply(A[i],[b[index_O] for b in B]),[beta[t+1] for beta in betas])
+                if t == 0:
+                    print(f"time {t+1} state {Q[i]} beta is {betas[i,t]:.4f}")
+
+        index_first = V.index(O[0]) # 第一个观测在观测集合中的索引
+        self.beats = betas
+        p = np.dot(np.multiply(PI,[b[index_first] for b in B]),[beta[0] for beta in betas])
+        self.backward_p = p
+        print(f"pi is {PI}")
+        print(f"finally p is {self.backward_p:.4f}")
+
+    def viterbi(self, Q, V, A, B, O, PI):
+        N = len(Q) # 隐藏状态序列长度
+        M = len(O) # 观测状态序列长度
+        deltas = np.zeros((N, M)) # 局部状态1
+        I = np.zeros(M) # 初始化最优路径矩阵，该矩阵维度与观测序列维度相同
+        psis = np.zeros((N, M)) # 初始化psis
+        # 算法原理 https://s1.ax1x.com/2020/10/21/BPut3D.png
+        # 计算例子 https://s1.ax1x.com/2020/10/21/BPuWuj.jpg
+        # 代码逻辑
+        for t in range(M):
+            index_O = V.index(O[t])
+            print(f"time {t + 1} and observe state is {O[t]}")  # 因为这边python的索引是从0开始的
+            for i in range(N):
+                if t == 0: # 初始化局部状态
+                    deltas[i,t] = PI[i] * B[i,index_O]
+                    psis[i,t] = 0
+                else:
+                    deltas[i, t] = np.max(np.multiply([delta[t - 1] for delta in deltas],
+                                                      [a[i] for a in A])) * B[i,index_O]
+                    psis[i,t] = np.argmax(np.multiply([delta[t - 1] for delta in deltas],
+                                                      [a[i] for a in A]))
+                print(f"hidden state {i+1} max deltas is {deltas[i, t]:.4f} and psis is {psis[i, t]}")
+
+        I[M - 1] = np.argmax([delta[M - 1] for delta in deltas]) # 最后时刻，概率最大的隐状态
+        for t in range(M - 2, -1, -1): # 递归由后向前得到其他结点
+            I[t] = psis[int(I[t+1]),t+1]
+
+        print(f"finally sequence is {I+1}") # python的索引是从0开始的
+
+
+
 
 
 
